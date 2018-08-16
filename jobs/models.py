1f4567259601e03f0642caaf0047180aa4a0f1a4
Yuvaraj Thiagarajan <netizen@email.com>
2018-08-15 18:24:05 -0700

    Database-driven Portfolio web application written in Python using Django and Bootstrap to showcase my resume, personal projects and blogs.

---
+++ jobs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Job(models.Model):
+    # image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
